c082120533b770113b46e38d7d5e23da7a0c34ad
Oliver Bachmann <obachmann@ymail.com>
2023-10-22 14:31:13 +0200

    Skipping fields if user_doc fields not set

--- whoisonline.py
+++ whoisonline.py
@@ -12,7 +12,7 @@ class WhoIsOnline:
         self.util.sid_data.setStartY(0)
 
         self.util.output("Who is Online:", 6, 0)
-
+        self.util.goto_next_line()
         for index, (sid, sid_data) in enumerate(self.util.all_sid_data.items()):
             user_doc = sid_data.user_document
             
@@ -20,6 +20,14 @@ class WhoIsOnline:
             if not user_doc:
                 continue
 
+            if not user_doc['age']:
+                user_doc['age']='?'
+            if not user_doc['sex']:
+                user_doc['sex']='?'
+            if not user_doc['hobbies']:
+                user_doc['hobbies']='?'
+            
+                
             display_str = f"{index + 1}. Username: {user_doc['username']}, Age: {user_doc['age']}, Sex: {user_doc['sex']}, Hobbies: {user_doc['hobbies']}"
 
             self.util.output(display_str, 6, 0)
